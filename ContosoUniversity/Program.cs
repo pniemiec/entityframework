39f248cab16f0d17cdf9fcb2c483451e45dcf9cb
pniemiec <33729918+pniemiec@users.noreply.github.com>
2017-11-16 20:41:55 +0100

    Lesson 01

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -7,14 +7,34 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using ContosoUniversity.Data;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ContosoUniversity
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<SchoolContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
